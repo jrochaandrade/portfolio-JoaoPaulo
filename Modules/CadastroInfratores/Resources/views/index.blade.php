eecc0014ad6264e93992a6c0a5f12eb4f06901d1
jrochaandrade <jrochaandrade@gmail.com>
2023-10-01 09:35:13 -0400

    Ajustes de criação

--- Modules/CadastroInfratores/Resources/views/index.blade.php
+++
@@ -1,9 +0,0 @@
-@extends('cadastroinfratores::layouts.master')
-
-@section('content')
-    <h1>Hello World</h1>
-
-    <p>
-        This view is loaded from module: {!! config('cadastroinfratores.name') !!}
-    </p>
-@endsection
---
+++ Modules/InteractiveMap/Routes/web.php
@@ -0,0 +1,21 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+use Illuminate\Support\Facades\Route;
+use Modules\InteractiveMap\Http\Controllers\InteractiveMapController;
+
+/* Route::prefix('interactivemap')->group(function() {
+    Route::get('/', 'InteractiveMapController@index');
+}); */
+
+Route::get('/mapa', [InteractiveMapController::class, 'index'])->name('mapa.index');
