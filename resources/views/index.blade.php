d4d99b619e0ca5d7e3aa89d92e1997b9dcd801a1
jrochaandrade <jrochaandrade@gmail.com>
2023-12-21 14:20:42 -0400

    Ajuste

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -9,10 +9,8 @@
 <div class="home">
     <div class="text">
         <div class="container-fluid content">
-            <h1>
-                Em desenvolvimento! 
-                <i class="fa-solid fa-person-digging"></i>
-            </h1> 
+            <h1>Em desenvolvimento!</h1> 
+            <img src="{{asset('gifs/const.gif')}}" alt="">
         </div>
     </div>
 </div>
