b12af30a9b5ca7357d67a035423087ef9dbacf69
jrochaandrade <jrochaandrade@gmail.com>
2024-05-30 17:05:25 -0400

    Ajuste index

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -19,18 +19,23 @@
                     <i class="bi bi-sun icon sun" id="sun"></i>
                     <i class="bi bi-moon icon moon" id="moon"></i>
                     <a href="#"><i class="bi bi-list btnMenu bi-3x"></i></i></a>
-                    
+
                 </div>
             </div>
-            <div class="divMain container">            
-                <section class="divImageMain row">
-                    <dir class="imageMain col-sm-6">
-                        <img src="{{ asset('images/Foto-Jp-SF.png')}}" alt="">
-                    </dir>
-                    <div class="textMain col-sm-6">
-                        <h3>João Paulo da Rocha Andrade</h3>
-                        <p>Formado em Análise e Desenvolvimento de Sistemas e cursando Engenharia de Software. Apaixonado por tecnologia, tenho experiência em HTML, CSS, JavaScript, PHP (Laravel) e SQL. Estou sempre buscando novas tecnologias e aprimorando minhas habilidades para desenvolver soluções inovadoras e eficientes.</p>
-                    </div>                    
+            <div class="divMain container">
+                <section class="divImageMain">
+                    <div class="row">
+                        <div class="imageMain col-sm-6">
+                            <img src="{{ asset('images/Foto-Jp-SF.png')}}" alt="">
+                        </div>
+                        <div class="textMain col-sm-6">
+                            <h3>João Paulo da Rocha Andrade</h3>
+                            <p>Formado em Análise e Desenvolvimento de Sistemas e cursando Engenharia de Software.
+                                Apaixonado por tecnologia, tenho experiência em HTML, CSS, JavaScript, PHP (Laravel) e
+                                SQL. Estou sempre buscando novas tecnologias e aprimorando minhas habilidades para
+                                desenvolver soluções inovadoras e eficientes.</p>
+                        </div>
+                    </div>
                 </section>
                 <section class="sectionProject1">
                     <h3>Mapa interativo</h3>
@@ -39,41 +44,57 @@
                             <img src="{{ asset('images/Mapa2.png') }}" alt="">
                         </div>
                         <div class="divTextProject1 col-sm-5">
-                            <p>Desenvolvi um sistema de mapa interativo em Laravel para gerenciar e visualizar dados geoespaciais de embargos. A aplicação permite o upload de arquivos KML, extrai os dados e coordenadas, e salva todas as informações no banco de dados. Os dados são exibidos em um mapa interativo e é possível realizar busca, filtragem, CRUD de embargos, e download dos dados em KML. A interface é intuitiva e responsiva, facilitando a navegação e manipulação dos dados geoespaciais.</p>
+                            <p>Desenvolvi um sistema de mapa interativo em Laravel para gerenciar e visualizar dados
+                                geoespaciais de embargos. A aplicação permite o upload de arquivos KML, extrai os dados
+                                e coordenadas, e salva todas as informações no banco de dados. Os dados são exibidos em
+                                um mapa interativo e é possível realizar busca, filtragem, CRUD de embargos, e download
+                                dos dados em KML. A interface é intuitiva e responsiva, facilitando a navegação e
+                                manipulação dos dados geoespaciais.</p>
                         </div>
-                    </div>                    
+                    </div>
                 </section>
-                <section class="sectionProject1">
+                <section class="sectionProject2">
                     <h3>Mapa interativo</h3>
                     <div class="divProjectsMain2 row">
                         <div class="divTextProject2 col-sm-5">
-                            <p>Desenvolvi um sistema de mapa interativo em Laravel para gerenciar e visualizar dados geoespaciais de embargos. A aplicação permite o upload de arquivos KML, extrai os dados e coordenadas, e salva todas as informações no banco de dados. Os dados são exibidos em um mapa interativo e é possível realizar busca, filtragem, CRUD de embargos, e download dos dados em KML. A interface é intuitiva e responsiva, facilitando a navegação e manipulação dos dados geoespaciais.</p>
+                            <p>Desenvolvi um sistema de mapa interativo em Laravel para gerenciar e visualizar dados
+                                geoespaciais de embargos. A aplicação permite o upload de arquivos KML, extrai os dados
+                                e coordenadas, e salva todas as informações no banco de dados. Os dados são exibidos em
+                                um mapa interativo e é possível realizar busca, filtragem, CRUD de embargos, e download
+                                dos dados em KML. A interface é intuitiva e responsiva, facilitando a navegação e
+                                manipulação dos dados geoespaciais.</p>
                         </div>
                         <div class="divImageProject2 col-sm-7" id="divImageProject2">
                             <img src="{{ asset('images/Mapa2.png') }}" alt="">
                         </div>
-                    </div>                    
+                    </div>
                 </section>
 
                 <section class="sectionProject1">
                     <h3>Mapa interativo</h3>
-                    <div class="divProjectsMain2 row">
-                        <div class="divTextProject2 col-sm-5">
-                            <p>Desenvolvi um sistema de mapa interativo em Laravel para gerenciar e visualizar dados geoespaciais de embargos. A aplicação permite o upload de arquivos KML, extrai os dados e coordenadas, e salva todas as informações no banco de dados. Os dados são exibidos em um mapa interativo e é possível realizar busca, filtragem, CRUD de embargos, e download dos dados em KML. A interface é intuitiva e responsiva, facilitando a navegação e manipulação dos dados geoespaciais.</p>
-                        </div>
-                        <div class="divImageProject2 col-sm-7" id="divImageProject2">
+                    <div class="divProjectsMain1 row">
+                        <div class="divImageProject1 col-sm-7" id="divImageProject2">
                             <img src="{{ asset('images/Mapa2.png') }}" alt="">
                         </div>
-                    </div>                    
+                        <div class="divTextProject1 col-sm-5">
+                            <p>Desenvolvi um sistema de mapa interativo em Laravel para gerenciar e visualizar dados
+                                geoespaciais de embargos. A aplicação permite o upload de arquivos KML, extrai os dados
+                                e coordenadas, e salva todas as informações no banco de dados. Os dados são exibidos em
+                                um mapa interativo e é possível realizar busca, filtragem, CRUD de embargos, e download
+                                dos dados em KML. A interface é intuitiva e responsiva, facilitando a navegação e
+                                manipulação dos dados geoespaciais.</p>
+                        </div>
+                        
+                    </div>
                 </section>
-            
-            
-            
-                
-            
-            
-            
-                
+
+
+
+
+
+
+
+
             </div>
         </div>
     </div>
