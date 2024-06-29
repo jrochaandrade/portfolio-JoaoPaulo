7a0c5f56dcdc64bc4645b8673e7ddc2f3e55400f
jrochaandrade <jrochaandrade@gmail.com>
2024-06-29 13:29:58 -0400

    Ajuste bloqueiro outras paginas

--- Modules/InteractiveMap/Routes/web.php
+++ Modules/InteractiveMap/Routes/web.php
@@ -14,7 +14,9 @@
 use Illuminate\Support\Facades\Route;
 use Modules\InteractiveMap\app\Http\Controllers\InteractiveMapController;
 
-Route::prefix('mapa')->group(function() {
+
+
+/* Route::prefix('mapa')->group(function() {
     Route::get('/', [InteractiveMapController::class, 'index'])->name('mapa.index');
     Route::post('/uploadKml', [InteractiveMapController::class, 'uploadKml'])->name('uploadKml');
     Route::get('/show/{id}', [InteractiveMapController::class, 'show'])->name('mapa.show');
@@ -22,4 +24,19 @@ Route::prefix('mapa')->group(function() {
     Route::put('/update/{id}', [InteractiveMapController::class, 'update'])->name('mapa.update');
     Route::get('/download/{id}', [InteractiveMapController::class, 'downloadPolygon'])->name('mapa.download');
     Route::delete('/delete/{id}', [InteractiveMapController::class, 'destroy'])->name('mapa.delete');
+});
+ */
+
+Route::group(['middleware' => ['auth']], function() /* colocando as rotas dentro dessa rota vai proteger do acesso via endereço */
+{
+    Route::prefix('mapa')->group(function() {
+        Route::get('/', [InteractiveMapController::class, 'index'])->name('mapa.index');
+        Route::post('/uploadKml', [InteractiveMapController::class, 'uploadKml'])->name('uploadKml');
+        Route::get('/show/{id}', [InteractiveMapController::class, 'show'])->name('mapa.show');
+        Route::get('/edit/{id}', [InteractiveMapController::class, 'edit'])->name('mapa.edit');
+        Route::put('/update/{id}', [InteractiveMapController::class, 'update'])->name('mapa.update');
+        Route::get('/download/{id}', [InteractiveMapController::class, 'downloadPolygon'])->name('mapa.download');
+        Route::delete('/delete/{id}', [InteractiveMapController::class, 'destroy'])->name('mapa.delete');
+    });
+    
 });
