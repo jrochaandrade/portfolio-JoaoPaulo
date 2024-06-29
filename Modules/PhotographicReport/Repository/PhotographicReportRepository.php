42eaca0ef0cd3e7f59c4d4b06e0ee15b74c68a06
jrochaandrade <jrochaandrade@gmail.com>
2024-06-29 14:36:25 -0400

    Ajuste filtro

--- Modules/PhotographicReport/Repository/PhotographicReportRepository.php
+++ Modules/PhotographicReport/Repository/PhotographicReportRepository.php
@@ -10,7 +10,7 @@ class PhotographicReportRepository extends PhotographicReport
     {
         $reports = PhotographicReport::select('photographic_reports.*')->when($request->search_data, function($query, $role) use($request){
             return $query->where(function($query) use($request){
-                $query->where('user', 'LIKE', "%$request->search_data%")->orWhere('Operation', 'LIKE', "%$request->search_data%")->orWhere('created_at', 'LIKE', "%$request->search_data%");
+                $query->where('user', 'LIKE', "%$request->search_data%")->orWhere('Operation', 'LIKE', "%$request->search_data%")->orWhere('id', 'LIKE', "%$request->search_data%");
             });
         });
 
